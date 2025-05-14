27e68bc3876c0a73d39664222dc3045ec553b694
worapolburaphan <worapol.pan.buraphan@gmail.com>
2025-05-15 01:24:47 +0700

    refactor: improve type equality checks and add additional test cases

--- src/type-conditional.d.ts
+++ src/type-conditional.d.ts
@@ -1,9 +1,11 @@
 declare type If<C extends boolean, T, F> = C extends true ? T : F;
-declare type IsExtend<X, Y> = X extends Y ? true : false;
 
 declare type Equal<X, Y> = 
-  (<T>() => If<IsExtend<T,X>,1,2>) extends
-  (<T>() => If<IsExtend<T,Y>,1,2>) ? true : false;
+  (<T>() => T extends X ? 1 : 2) extends
+  (<T>() => T extends Y ? 1 : 2) ? true : false;
 
 type TestShouldBeNotEqual = AssertEqual<Equal<"b", "a">, false>;
 type TestShouldBeEqual = AssertEqual<Equal<"b", "b">, true>;
+type TestShouldBeObjectEqual = AssertEqual<Equal<{a: "b", b: "c"}, {a: "b", b: "c"}>, true>;
+type TestShouldBeNotEqual2 = AssertNot<Equal<{"hello": "world"}, "c">>;
+type TestShouldBeObjectNotEqual = AssertNot<Equal<{a: "b", b: "c"}, {a: "b"}>>;
