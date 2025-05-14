3dc3f2c1cb68d682113c997c8837ef72b792a9b1
worapolburaphan <worapol.pan.buraphan@gmail.com>
2025-05-15 03:02:52 +0700

    fix: lint

--- src/type-conditional.d.ts
+++ src/type-conditional.d.ts
@@ -1,11 +1,18 @@
-declare type Equal<X, Y> = 
-  (<T>() => T extends X ? 1 : 2) extends
-  (<T>() => T extends Y ? 1 : 2) ? true : false;
+declare type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends <
+  T
+>() => T extends Y ? 1 : 2
+  ? true
+  : false;
 
-declare type True<T extends true> = T
+declare type True<T extends true> = T;
 
 type TestShouldBeNotEqual = AssertEqual<Equal<"b", "a">, false>;
 type TestShouldBeEqual = AssertEqual<Equal<"b", "b">, true>;
-type TestShouldBeObjectEqual = AssertEqual<Equal<{a: "b", b: "c"}, {a: "b", b: "c"}>, true>;
-type TestShouldBeNotEqual2 = AssertNot<Equal<{"hello": "world"}, "c">>;
-type TestShouldBeObjectNotEqual = AssertNot<Equal<{a: "b", b: "c"}, {a: "b"}>>;
+type TestShouldBeObjectEqual = AssertEqual<
+  Equal<{ a: "b"; b: "c" }, { a: "b"; b: "c" }>,
+  true
+>;
+type TestShouldBeNotEqual2 = AssertNot<Equal<{ hello: "world" }, "c">>;
+type TestShouldBeObjectNotEqual = AssertNot<
+  Equal<{ a: "b"; b: "c" }, { a: "b" }>
+>;
