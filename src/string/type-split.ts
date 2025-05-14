729b8390b4be358fbd6f9ae1055ebafa1869e852
worapolburaphan <worapol.pan.buraphan@gmail.com>
2025-05-15 03:28:01 +0700

    fix: rename and feature of dot separate to split

--- src/string/type-split.ts
+++ src/string/type-split.ts
@@ -1,6 +1,6 @@
 declare type Split<
   T extends string,
-  S extends string = "."
+  S extends string = ","
 > = T extends `${infer L}${S}${infer Rest}`
   ? L extends ""
     ? Split<Rest, S>
@@ -8,36 +8,36 @@ declare type Split<
   : ExcludeEmpty<T>;
 
 type TestSplitMustBeCorrect = AssertEqual<
-  Split<"a.b.c">,
+  Split<"a,b,c">,
   "a" | "b" | "c"
 >;
 
 type TestSplitMustReturnUniqueType = AssertEqual<
-  Split<"b.a.b.a">,
+  Split<"b,a,b,a">,
   "a" | "b"
 >;
 
-type TestShouldBeSkipEmptyName = AssertEqual<Split<"a..">, "a">;
-type TestShouldBeSkipEmptyName2 = AssertEqual<Split<"a.b..">, "a" | "b">;
+type TestShouldBeSkipEmptyName = AssertEqual<Split<"a,">, "a">;
+type TestShouldBeSkipEmptyName2 = AssertEqual<Split<"a,b,">, "a" | "b">;
 type TestShouldBeSkipEmptyName3 = AssertEqual<
-  Split<"..a.b.c">,
+  Split<",a,b,c">,
   "a" | "b" | "c"
 >;
 type TestShouldBeSkipEmptyName4 = AssertEqual<
-  Split<"a.b.c..">,
+  Split<"a,b,c,">,
   "a" | "b" | "c"
 >;
 type TestShouldBeSkipEmptyName5 = AssertEqual<
-  Split<".a..b.c.">,
+  Split<",a,b,c,">,
   "a" | "b" | "c"
 >;
 type TestShouldBeSkipEmptyName6 = AssertEqual<
-  Split<".a.b...d.e.e.a.c..">,
+  Split<",a,b,d,e,e,a,c,">,
   "a" | "b" | "c" | "d" | "e"
 >;
 
 type TestSplitWithCommaSeparator = AssertEqual<
-  Split<"a,b,c", ",">,
+  Split<"a.b.c", ".">,
   "a" | "b" | "c"
 >;
 
